3bfd39769757f03d2c30d5ce16dadd7cb5442d49
nortoxs <vivekkumar89751@gmail.com>
2024-12-16 01:11:51 +0530

    Update quizx.py
    Signed-off-by: nortoxs <vivekkumar89751@gmail.com>

--- AnonXMusic/plugins/tools/quizx.py
+++ AnonXMusic/plugins/tools/quizx.py
@@ -1,21 +1,21 @@
+from AnonXMusic import botx
 import asyncio
 from telethon import TelegramClient, events, types
-from telethon.tl.functions.messages import SendPoll
-from telethon.tl.types import InputPeerUser
-from AnonXMusic import botx
+from telethon.tl.types import PollAnswer
 
 # Define your poll question and options
 QUESTION = "What is the capital of France?"
 OPTIONS = ["Berlin", "Madrid", "Paris", "Rome"]
 CORRECT_OPTION_ID = 2  # Index for "Paris"
 
+
 # Handler for the /quiz command
 @botx.on(events.NewMessage(pattern='/quiz'))
 async def send_quiz(event):
     # Sending a poll (quiz) to the user
-    poll = await event.respond(
+    await event.respond(
         QUESTION,
-        buttons=[types.KeyboardButton(option) for option in OPTIONS],
+        buttons=None,  # No inline buttons for now
         poll=types.Poll(
             question=QUESTION,
             answers=[types.PollAnswer(option) for option in OPTIONS],
@@ -34,4 +34,3 @@ async def handle_poll_answer(event):
         await event.reply("Correct!")
     else:
         await event.reply("Incorrect!")
-        
