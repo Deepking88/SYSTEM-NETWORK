4888dbf26a01987e9074e6c5a6a1388d01f5d446
nortoxs <vivekkumar89751@gmail.com>
2024-12-15 22:32:56 +0530

    Update quizx.py
    Signed-off-by: nortoxs <vivekkumar89751@gmail.com>

--- AnonXMusic/plugins/tools/quizx.py
+++ AnonXMusic/plugins/tools/quizx.py
@@ -1,10 +1,10 @@
 # Define the poll sending function
 import asyncio
 from AnonXMusic import app 
-from pyrogram import Client, filters, Message 
+from pyrogram import Client, filters 
 
 @app.on_message()
-async def send_poll(app: client, Message: message):
+async def send_poll(client, message):
     if message.text == "/poll":  # Trigger the poll with a specific command
         await app.send_poll(
             chat_id=message.chat.id,
