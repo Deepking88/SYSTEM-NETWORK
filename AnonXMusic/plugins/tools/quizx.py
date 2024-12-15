d1a42df7bf1dd874666b233db1dd70edcf5c31cf
nortoxs <vivekkumar89751@gmail.com>
2024-12-15 22:41:25 +0530

    Update quizx.py
    Signed-off-by: nortoxs <vivekkumar89751@gmail.com>

--- AnonXMusic/plugins/tools/quizx.py
+++ AnonXMusic/plugins/tools/quizx.py
@@ -1,15 +1,16 @@
-# Define the poll sending function
-import asyncio
 from AnonXMusic import app 
-from pyrogram import Client, filters 
-from pyrogram.types import PollOption
+from pyrogram import Client
 
 @app.on_message()
 async def send_poll(client, message):
-    if message.text == "/poll":  # Trigger the poll with a specific command
-        await app.send_poll(
-            chat_id=message.chat.id,
-            question="What's your favorite programming language?",
-            options=["Python", "JavaScript", "C++", "Java"],
-            is_anonymous=True  # Set to False for non-anonymous polls
+    if message.text == "/poll":  # Command to trigger poll creation
+        # Define options as a list of strings
+        options = ["Option 1", "Option 2", "Option 3"]  # List of answer options
+
+        # Send the poll with the given question and options
+        await client.send_poll(
+            chat_id=message.chat.id,  # The chat ID where the poll will be sent
+            question="What's your favorite option?",  # The poll question
+            options=options,  # The list of options as strings
+            is_anonymous=True  # Whether the poll is anonymous or not
         )
