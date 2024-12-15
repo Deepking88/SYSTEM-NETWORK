b7d42c4d77b37cd7eb3f82de3ecc826597ac083b
nortoxs <vivekkumar89751@gmail.com>
2024-12-15 22:31:41 +0530

    Update quizx.py
    Signed-off-by: nortoxs <vivekkumar89751@gmail.com>

--- AnonXMusic/plugins/tools/quizx.py
+++ AnonXMusic/plugins/tools/quizx.py
@@ -3,9 +3,9 @@ from AnonXMusic import app
 from pyrogram import Client, filters, Message 
 
 @app.on_message()
-async def send_poll(client, Message: message):
+async def send_poll(app: client, Message: message):
     if message.text == "/poll":  # Trigger the poll with a specific command
-        await client.send_poll(
+        await app.send_poll(
             chat_id=message.chat.id,
             question="What's your favorite programming language?",
             options=["Python", "JavaScript", "C++", "Java"],
