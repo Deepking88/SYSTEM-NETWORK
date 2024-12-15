ec1fdfd649582c168058d4fc858043113aee4d35
nortoxs <vivekkumar89751@gmail.com>
2024-12-15 22:48:20 +0530

    Update quizx.py
    Signed-off-by: nortoxs <vivekkumar89751@gmail.com>

--- AnonXMusic/plugins/tools/quizx.py
+++ AnonXMusic/plugins/tools/quizx.py
@@ -1,16 +1,12 @@
-from AnonXMusic import app 
-from pyrogram import Client
+from pyrogram import Client, filters
+from AnonXMusic import app
 
-@app.on_message()
+# Define the function to send a poll
+@app.on_message(filters.command("poll"))
 async def send_poll(client, message):
-    if message.text == "/poll":  # Command to trigger poll creation
-        # Define options as a list of strings
-        options = ["Option 1", "Option 2", "Option 3"]  # List of answer options
-
-        # Send the poll with the given question and options
-        await client.send_poll(
-            chat_id=message.chat.id,  # The chat ID where the poll will be sent
-            question="What's your favorite option?",  # The poll question
-            options=options,  # The list of options as strings
-            is_anonymous=True  # Whether the poll is anonymous or not
-        )
+    # Sending a poll when the "/send_poll" command is received
+    await app.send_poll(
+        chat_id=message.chat.id,  # The chat ID (group or private chat)
+        question="Is this a poll question?",  # Poll question
+        options=["Yes", "No", "Maybe"]  # Poll options
+    )
