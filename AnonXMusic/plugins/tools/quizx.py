8807f2640d6e18e7e91b2851390072ac343c464e
nortoxs <vivekkumar89751@gmail.com>
2024-12-16 01:19:14 +0530

    Update quizx.py
    Signed-off-by: nortoxs <vivekkumar89751@gmail.com>

--- AnonXMusic/plugins/tools/quizx.py
+++ AnonXMusic/plugins/tools/quizx.py
@@ -1,19 +1,20 @@
 import asyncio
 from telethon import TelegramClient, events, types
-from AnonXMusic import botx
+from AnonXMusic import botx 
 
 # Define your poll question and options
 QUESTION = "What is the capital of France?"
 OPTIONS = ["Berlin", "Madrid", "Paris", "Rome"]
 CORRECT_OPTION_ID = 2  # Index for "Paris"
 
+
 # Handler for the /quiz command
 @botx.on(events.NewMessage(pattern='/quiz'))
 async def send_quiz(event):
     # Sending a poll (quiz) to the user
-    await event.respond(
+    poll = await event.respond(
         QUESTION,
-        buttons=None,  # No inline buttons for now
+        buttons=None,  # Optional: can be used for inline buttons, not required for poll
         poll=types.Poll(
             question=QUESTION,
             answers=[types.PollAnswer(option) for option in OPTIONS],
@@ -24,11 +25,12 @@ async def send_quiz(event):
     )
     await event.reply("Quiz sent!")
 
-# Correct event to handle poll answers (using events.PollAnswer)
-@botx.on(events.PollAnswer)
+# Handler to listen for incoming poll answers
+@botx.on(events.NewMessage)
 async def handle_poll_answer(event):
-    answer = event.poll_answer
-    if answer.selected_id == CORRECT_OPTION_ID:
-        await event.reply("Correct!")
-    else:
-        await event.reply("Incorrect!")
+    if event.poll is not None:
+        answer = event.poll
+        if event.message.poll_answer.selected_id == CORRECT_OPTION_ID:
+            await event.reply("Correct!")
+        else:
+            await event.reply("Incorrect!")
