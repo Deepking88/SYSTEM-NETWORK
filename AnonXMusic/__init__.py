e789186faab0faafd82c0dfeab6edc43cea290d1
DHPR <133486266+Deepking88@users.noreply.github.com>
2025-01-07 15:37:04 +0530

    Update __init__.py
    Signed-off-by: DHPR  <133486266+Deepking88@users.noreply.github.com>

--- AnonXMusic/__init__.py
+++ AnonXMusic/__init__.py
@@ -3,10 +3,8 @@ from AnonXMusic.core.dir import dirr
 from AnonXMusic.core.git import git
 from AnonXMusic.core.userbot import Userbot
 from AnonXMusic.misc import dbb, heroku
-from telethon import TelegramClient, events
+
 from .logging import LOGGER
-from config import *
-from telegram.ext import Application
 
 dirr()
 git()
@@ -16,10 +14,6 @@ heroku()
 app = Anony()
 userbot = Userbot()
 
-# Initialize the bot application
-botxx = Application.builder().token(BOT_TOKEN).build()
-
-botx = TelegramClient('bot', api_id=API_ID, api_hash=API_HASH).start(bot_token=BOT_TOKEN)
 
 from .platforms import *
 
