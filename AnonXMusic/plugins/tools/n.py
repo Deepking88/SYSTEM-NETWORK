eea7aa9e70319290df748dd8fa8fa71e2475c670
Uffy-xD <vivansingh8372@outlook.com>
2025-01-31 23:08:12 +0530

    Update n.py
    Signed-off-by: Uffy-xD <vivansingh8372@outlook.com>

--- AnonXMusic/plugins/tools/n.py
+++ AnonXMusic/plugins/tools/n.py
@@ -16,6 +16,7 @@ MONGO_DB_URI = os.getenv("MONGO_DB_URI", "mongodb+srv://jay:jay@jay.r2lxx.mongod
 # MongoClient to interact with MongoDB
 client = MongoClient(MONGO_DB_URI)
 
+
 # Backup MongoDB database function (used with /export)
 def backup_mongo_db():
     try:
@@ -43,8 +44,8 @@ def restore_mongo_db(backup_file_path):
         print(f"Error during restore: {e}")
         return False
 
-# Command handler for /import
-@app.on_message(filters.command("nimport"))
+# Command handler for /import (process backup file sent by user)
+@app.on_message(filters.command("import"))
 async def import_backup(client, message: Message):
     # Check if a file is provided with the command
     if message.document:
@@ -59,10 +60,11 @@ async def import_backup(client, message: Message):
         else:
             await message.reply("There was an error while importing the backup.")
     else:
+        # If no document was sent, provide an explanation about the command usage
         await message.reply("Please send a valid MongoDB backup file with the /import command.")
 
 # Command handler for /export (backup command)
-@app.on_message(filters.command("nexport"))
+@app.on_message(filters.command("export"))
 async def export_database(client, message: Message):
     # Perform MongoDB backup
     backup_data = backup_mongo_db()
